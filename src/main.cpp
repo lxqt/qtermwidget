3a8cdc68ee8df623f35d0494b6246618bf80024a
Jerome Leclanche <jerome@leclan.ch>
2014-10-21 17:31:46 +0200

    CMakeLists.txt cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@
     modify it under the terms of the GNU Library General Public
     License as published by the Free Software Foundation; either
     version 2 of the License, or (at your option) any later version.
-		
+
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Library General Public License for more details.
-				
+
     You should have received a copy of the GNU Library General Public License
     along with this library; see the file COPYING.LIB.  If not, write to
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
@@ -30,16 +30,16 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     QIcon::setThemeName("oxygen");
     QMainWindow *mainWindow = new QMainWindow();
-   
+
     QTermWidget *console = new QTermWidget();
- 
+
     QMenuBar *menuBar = new QMenuBar(mainWindow);
     QMenu *actionsMenu = new QMenu("Actions", menuBar);
     menuBar->addMenu(actionsMenu);
     actionsMenu->addAction("Find..", console, SLOT(toggleShowSearchBar()), QKeySequence(Qt::CTRL + Qt::SHIFT + Qt::Key_F));
     actionsMenu->addAction("About Qt", &app, SLOT(aboutQt()));
-    mainWindow->setMenuBar(menuBar);       
-    
+    mainWindow->setMenuBar(menuBar);
+
     QFont font = QApplication::font();
 #ifdef Q_WS_MAC
     font.setFamily("Monaco");
@@ -49,9 +49,9 @@ int main(int argc, char *argv[])
     font.setFamily("Monospace");
 #endif
     font.setPointSize(12);
-    
+
     console->setTerminalFont(font);
-    
+
    // console->setColorScheme(COLOR_SCHEME_BLACK_ON_LIGHT_YELLOW);
     console->setScrollBarPosition(QTermWidget::ScrollBarRight);
 
@@ -62,21 +62,20 @@ int main(int argc, char *argv[])
         if (console->availableKeyBindings().contains(arg))
             console->setKeyBindings(arg);
     }
-    
+
     mainWindow->setCentralWidget(console);
     mainWindow->resize(600, 400);
-    
+
     // info output
     qDebug() << "* INFO *************************";
     qDebug() << " availableKeyBindings:" << console->availableKeyBindings();
     qDebug() << " keyBindings:" << console->keyBindings();
     qDebug() << " availableColorSchemes:" << console->availableColorSchemes();
     qDebug() << "* INFO END *********************";
-    
+
     // real startup
     QObject::connect(console, SIGNAL(finished()), mainWindow, SLOT(close()));
 
-    mainWindow->show();    
+    mainWindow->show();
     return app.exec();
-} 
-
+}
