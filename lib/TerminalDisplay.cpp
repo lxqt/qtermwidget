10e17968e4457da2b91675984e17009ee6e1e7aa
rago1975 <take1975@kumamoto.email.ne.jp>
2016-01-08 00:00:30 +0900

    Use function setWorldTranfer for Qpainter instead of setWorldMatrix

--- lib/TerminalDisplay.cpp
+++ lib/TerminalDisplay.cpp
@@ -1497,7 +1497,7 @@ void TerminalDisplay::drawContents(QPainter &paint, const QRect &rect)
          unistr.resize(p);
 
          // Create a text scaling matrix for double width and double height lines.
-         QMatrix textScale;
+         QTransform textScale;
 
          if (y < _lineProperties.size())
          {
@@ -1509,7 +1509,7 @@ void TerminalDisplay::drawContents(QPainter &paint, const QRect &rect)
          }
 
          //Apply text scaling matrix.
-         paint.setWorldMatrix(textScale, true);
+         paint.setWorldTransform(textScale, true);
 
          //calculate the area in which the text will be drawn
          QRect textArea = calculateTextArea(tLx, tLy, x, y, len);
@@ -1533,7 +1533,7 @@ void TerminalDisplay::drawContents(QPainter &paint, const QRect &rect)
          _fixedFont = save__fixedFont;
 
          //reset back to single-width, single-height _lines
-         paint.setWorldMatrix(textScale.inverted(), true);
+         paint.setWorldTransform(textScale.inverted(), true);
 
          if (y < _lineProperties.size()-1)
          {
