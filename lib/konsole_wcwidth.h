5d8544455fbf3f9608d03020182266fd90a72d39
Yen Chi Hsuan <yan12125@gmail.com>
2016-11-19 19:31:50 +0800

    Drop the ancient wcwidth impl. and use utf8proc if possible (#99)

--- lib/konsole_wcwidth.h
+++ lib/konsole_wcwidth.h
@@ -11,14 +11,9 @@
 #define _KONSOLE_WCWIDTH_H_
 
 // Qt
-#include <QtGlobal>
-
 class QString;
 
-int konsole_wcwidth(quint16 ucs);
-#if 0
-int konsole_wcwidth_cjk(Q_UINT16 ucs);
-#endif
+int konsole_wcwidth(wchar_t ucs);
 
 int string_width( const QString & txt );
 
