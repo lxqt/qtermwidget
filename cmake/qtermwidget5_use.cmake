f7e999c0e978ddf697c453673184a46f80f52632
Luís Pereira <luis.artur.pereira@gmail.com>
2017-02-28 15:47:41 +0000

    Use the CMake Targets way
    Drop the qtermwidget5_use file. Everything done with targets.

--- cmake/qtermwidget5_use.cmake
+++
@@ -1,9 +0,0 @@
-find_package(Qt5Widgets REQUIRED)
-
-include_directories(${Qt5Widgets_INCLUDE_DIRS})
-add_definitions(${Qt5Core_DEFINITIONS})
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${Qt5Widgets_EXECUTABLE_COMPILE_FLAGS}")
-set(QTERMWIDGET_QT_LIBRARIES ${Qt5Widgets_LIBRARIES})
-
-include_directories(${QTERMWIDGET_INCLUDE_DIRS})
-
