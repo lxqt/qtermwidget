bff639f9da4ec895e94da658d0796805767ff0c3
paiiou <paiiou@free.fr>
2017-04-01 10:18:58 +0200

    Fix french tralations

---
+++ qtermwidget_fr.ts
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>Konsole::TerminalDisplay</name>
+    <message>
+        <location filename="../../TerminalDisplay.cpp" line="3121"/>
+        <source>&lt;qt&gt;Output has been &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;suspended&lt;/a&gt; by pressing Ctrl+S.  Press &lt;b&gt;Ctrl+Q&lt;/b&gt; to resume.&lt;/qt&gt;</source>
+        <translation>&lt;qt&gt;La sortie a été &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;suspendue&lt;/a&gt; en pressant Ctrl+S.  Pressez &lt;b&gt;Ctrl+Q&lt;/b&gt; pour reprendre.&lt;/qt&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Konsole::Vt102Emulation</name>
+    <message>
+        <location filename="../../Vt102Emulation.cpp" line="1032"/>
+        <source>No keyboard translator available.  The information needed to convert key presses into characters to send to the terminal is missing.</source>
+        <translation>Aucun traducteur disponible.  L&apos;information nécessaire à la conversion des touches pressées en caractères à envoyer au terminal est absente.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../ColorScheme.cpp" line="278"/>
+        <location filename="../../ColorScheme.cpp" line="293"/>
+        <source>Un-named Color Scheme</source>
+        <translation>Schéma des couleurs non nommé</translation>
+    </message>
+    <message>
+        <location filename="../../ColorScheme.cpp" line="464"/>
+        <source>Accessible Color Scheme</source>
+        <translation>Schéma des couleur accessible</translation>
+    </message>
+    <message>
+        <location filename="../../Filter.cpp" line="516"/>
+        <source>Open Link</source>
+        <translation>Ouvrir le lien</translation>
+    </message>
+    <message>
+        <location filename="../../Filter.cpp" line="517"/>
+        <source>Copy Link Address</source>
+        <translation>Copier l&apos;adresse du lien</translation>
+    </message>
+    <message>
+        <location filename="../../Filter.cpp" line="521"/>
+        <source>Send Email To...</source>
+        <translation>Envoyer un courriel à ...</translation>
+    </message>
+    <message>
+        <location filename="../../Filter.cpp" line="522"/>
+        <source>Copy Email Address</source>
+        <translation>Copier l&apos;adresse du courriel</translation>
+    </message>
+</context>
+<context>
+    <name>QTermWidget</name>
+    <message>
+        <location filename="../../qtermwidget.cpp" line="428"/>
+        <source>Color Scheme Error</source>
+        <translation>Erreur du schéma des couleurs</translation>
+    </message>
+    <message>
+        <location filename="../../qtermwidget.cpp" line="429"/>
+        <source>Cannot load color scheme: %1</source>
+        <translation>Impossible de charger le schéma de couleurs : %1</translation>
+    </message>
+</context>
+<context>
+    <name>SearchBar</name>
+    <message>
+        <location filename="../../SearchBar.ui" line="14"/>
+        <source>SearchBar</source>
+        <translation>Barre de recherche</translation>
+    </message>
+    <message>
+        <location filename="../../SearchBar.ui" line="20"/>
+        <source>X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <location filename="../../SearchBar.ui" line="32"/>
+        <source>Find:</source>
+        <translation>Trouver :</translation>
+    </message>
+    <message>
+        <location filename="../../SearchBar.ui" line="42"/>
+        <source>&lt;</source>
+        <translation>&lt;</translation>
+    </message>
+    <message>
+        <location filename="../../SearchBar.ui" line="54"/>
+        <source>&gt;</source>
+        <translation>&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../SearchBar.ui" line="66"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../../SearchBar.cpp" line="39"/>
+        <source>Match case</source>
+        <translation>Sensible à la casse</translation>
+    </message>
+    <message>
+        <location filename="../../SearchBar.cpp" line="45"/>
+        <source>Regular expression</source>
+        <translation>Expression régulière</translation>
+    </message>
+    <message>
+        <location filename="../../SearchBar.cpp" line="49"/>
+        <source>Highlight all matches</source>
+        <translation>Surbrillance de toutes les concordances</translation>
+    </message>
+</context>
+</TS>
