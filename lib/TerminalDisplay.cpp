2b42af9478fb2b9430cec48321efbbacc3f73723
Chih-Hsuan Yen <yan12125@gmail.com>
2018-01-29 23:17:34 +0800

    Finish SGR mouse protocol (1006)
    Some codes in https://github.com/KDE/konsole/commit/c83e7b638dcc42b617a067e6dc23686eccf23b00
    are missing. Backport all of them.
    
    Fixes #164

--- lib/TerminalDisplay.cpp
+++ lib/TerminalDisplay.cpp
@@ -2324,9 +2324,9 @@ void TerminalDisplay::mouseReleaseEvent(QMouseEvent* ev)
       //       applies here, too.
 
       if (!_mouseMarks && !(ev->modifiers() & Qt::ShiftModifier))
-        emit mouseSignal( 3, // release
+        emit mouseSignal( 0,
                         charColumn + 1,
-                        charLine + 1 +_scrollBar->value() -_scrollBar->maximum() , 0);
+                        charLine + 1 +_scrollBar->value() -_scrollBar->maximum() , 2);
     }
     dragInfo.state = diNone;
   }
@@ -2336,10 +2336,10 @@ void TerminalDisplay::mouseReleaseEvent(QMouseEvent* ev)
        ((ev->button() == Qt::RightButton && !(ev->modifiers() & Qt::ShiftModifier))
                         || ev->button() == Qt::MidButton) )
   {
-    emit mouseSignal( 3,
+    emit mouseSignal( ev->button() == Qt::MidButton ? 1 : 2,
                       charColumn + 1,
                       charLine + 1 +_scrollBar->value() -_scrollBar->maximum() ,
-                      0);
+                      2);
   }
 }
 
