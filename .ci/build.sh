f15d44c1d92582af27ffc91a175f771cf087e6f0
Chih-Hsuan Yen <yan12125@gmail.com>
2020-06-13 12:15:38 +0800

    [WIP] Use Clazy in CI
    Test only. DO NOT MERGE.

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,5 +2,9 @@ set -ex
 
 mkdir build
 cd build
-cmake -DBUILD_EXAMPLE=ON -DQTERMWIDGET_BUILD_PYTHON_BINDING=ON ..
+CC=clang CXX=clang++ cmake \
+    -DBUILD_EXAMPLE=ON \
+    -DQTERMWIDGET_BUILD_PYTHON_BINDING=ON \
+    -DLXQT_ENABLE_CLAZY=ON \
+    ..
 make
