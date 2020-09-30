5f818d4f026aea1a7b2f540b3a3ed2f5a0423db8
Chih-Hsuan Yen <yan12125@gmail.com>
2020-09-30 20:57:24 +0800

    Fix building when utempter is enabled (#356)
    * Fix building when utempter is enabled
    
    And test it with CI to make sure it will not break in the future.
    
    Looks like libulog.so is provided by FreeBSD and libutempter.so is
    provided on Linux [1].
    
    Closes https://github.com/lxqt/qtermwidget/issues/351.
    
    [1] https://reviews.freebsd.org/D7989?id=20568
    
    * Restore the hint for FreeBSD packagers in the CMake option

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,5 +2,9 @@ set -ex
 
 mkdir build
 cd build
-cmake -DBUILD_EXAMPLE=ON -DQTERMWIDGET_BUILD_PYTHON_BINDING=ON ..
+cmake \
+    -DBUILD_EXAMPLE=ON \
+    -DQTERMWIDGET_BUILD_PYTHON_BINDING=ON \
+    -DQTERMWIDGET_USE_UTEMPTER=ON \
+    ..
 make
