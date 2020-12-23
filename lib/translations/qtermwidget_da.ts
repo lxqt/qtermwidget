abce10c83b464ff1d7dca4feb8785f1875f2f777
scootergrisen <scootergrisen@gmail.com>
2020-12-23 01:58:17 +0100

    Weblate commits
    Co-authored-by: scootergrisen <scootergrisen@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/da/
    Translation: LXQt Desktop/qtermwidget

--- lib/translations/qtermwidget_da.ts
+++ lib/translations/qtermwidget_da.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../Session.cpp" line="454"/>
         <source>Bell in session &apos;%1&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Bell i session &apos;%1&apos;</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="606"/>
         <source>Session &apos;%1&apos; exited with status %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>Session &apos;%1&apos; afsluttede med status %2.</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="608"/>
         <source>Session &apos;%1&apos; crashed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Session &apos;%1&apos; holdt op med at virke.</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="613"/>
         <source>Session &apos;%1&apos; exited unexpectedly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Session &apos;%1&apos; afsluttede uventet.</translation>
     </message>
 </context>
 <context>
