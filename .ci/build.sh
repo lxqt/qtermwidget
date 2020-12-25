5b1a15bef85af9b5c2e5c64dba201dd09e569c2b
Chih-Hsuan Yen <yan12125@gmail.com>
2020-12-25 21:11:57 +0800

    Move to GitHub Actions
    See https://github.com/lxqt/lxqt/issues/1644#issuecomment-751235417

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,10 +1,17 @@
 set -ex
 
-mkdir build
-cd build
-cmake \
+cat >> /etc/pacman.conf <<EOF
+[archlinuxcn]
+Server = https://repo.archlinuxcn.org/\$arch
+SigLevel = Never
+EOF
+
+pacman -Syu --noconfirm
+# See *depends in https://github.com/archlinuxcn/repo/blob/master/archlinuxcn/qtermwidget-git/PKGBUILD
+pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools python-pyqt5 python-sip sip
+
+cmake -B build -S . \
     -DBUILD_EXAMPLE=ON \
     -DQTERMWIDGET_BUILD_PYTHON_BINDING=ON \
-    -DQTERMWIDGET_USE_UTEMPTER=ON \
-    ..
-make
+    -DQTERMWIDGET_USE_UTEMPTER=ON
+make -C build
