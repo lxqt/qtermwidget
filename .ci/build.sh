3750f054561d7e317b06dbe5bcbf95277f8cc83d
Chih-Hsuan Yen <yan12125@gmail.com>
2021-01-09 01:48:43 +0800

    Fix building after some PyQt changes in Arch Linux
    See: https://github.com/archlinux/svntogit-community/commit/22706b6db6247d8a657332daa9c6bde62faa0d7a

--- .ci/build.sh
+++ .ci/build.sh
@@ -8,7 +8,7 @@ EOF
 
 pacman -Syu --noconfirm
 # See *depends in https://github.com/archlinuxcn/repo/blob/master/archlinuxcn/qtermwidget-git/PKGBUILD
-pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools python-pyqt5 python-sip sip
+pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools python-pyqt5 python-sip4 sip4
 
 cmake -B build -S . \
     -DBUILD_EXAMPLE=ON \
