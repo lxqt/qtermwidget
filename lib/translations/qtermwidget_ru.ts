4b8bf5eb6d648bd120f59354324363bc6b9fe1ca
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-01-19 18:50:46 +0100

    Translations update from Weblate (#400)
    * Weblate commits
    
    Co-authored-by: Stefonarch <stefonarch@riseup.net>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/it/
    Translation: LXQt Desktop/qtermwidget
    
    * Weblate commits
    
    Co-authored-by: Baptiste Huchon <baptistehuchon17@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/fr/
    Translation: LXQt Desktop/qtermwidget
    
    * Weblate commits
    
    Co-authored-by: Golubev Alexander <fatzer2@gmail.com>
    
    Co-authored-by: Stefonarch <stefonarch@riseup.net>
    Co-authored-by: Baptiste Huchon <baptistehuchon17@gmail.com>
    Co-authored-by: Golubev Alexander <fatzer2@gmail.com>

--- lib/translations/qtermwidget_ru.ts
+++ lib/translations/qtermwidget_ru.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr">
+<TS version="2.1" language="ru">
 <context>
     <name>Konsole::Session</name>
     <message>
         <location filename="../Session.cpp" line="454"/>
         <source>Bell in session &apos;%1&apos;</source>
-        <translation type="unfinished">Sonnerie dans la session &apos;%1&apos;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="606"/>
         <source>Session &apos;%1&apos; exited with status %2.</source>
-        <translation>La session &apos;%1&apos; s&apos;est terminée avec le statut %2.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="608"/>
         <source>Session &apos;%1&apos; crashed.</source>
-        <translation>La session &apos;%1&apos; a planté.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="613"/>
         <source>Session &apos;%1&apos; exited unexpectedly.</source>
-        <translation>La session &apos;%1&apos; s&apos;est fermée de manière inattendue.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -29,17 +29,17 @@
     <message>
         <location filename="../TerminalDisplay.cpp" line="1282"/>
         <source>Size: XXX x XXX</source>
-        <translation>Taille : XXX x XXX</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="1294"/>
         <source>Size: %1 x %2</source>
-        <translation>Taille : %1 x %2</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="3180"/>
         <source>&lt;qt&gt;Output has been &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;suspended&lt;/a&gt; by pressing Ctrl+S.  Press &lt;b&gt;Ctrl+Q&lt;/b&gt; to resume.&lt;/qt&gt;</source>
-        <translation>&lt;qt&gt;La sortie a été &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;suspendue&lt;/a&gt; en pressant Ctrl+S.  Presser &lt;b&gt;Ctrl+Q&lt;/b&gt; pour reprendre.&lt;/qt&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -47,7 +47,7 @@
     <message>
         <location filename="../Vt102Emulation.cpp" line="1113"/>
         <source>No keyboard translator available.  The information needed to convert key presses into characters to send to the terminal is missing.</source>
-        <translation>Aucun traducteur de clavier disponible. Les informations nécessaires pour convertir les touches utilisées en caractères à envoyer au terminal sont manquantes.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -56,32 +56,32 @@
         <location filename="../ColorScheme.cpp" line="278"/>
         <location filename="../ColorScheme.cpp" line="293"/>
         <source>Un-named Color Scheme</source>
-        <translation>Jeu de couleurs non nommé</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ColorScheme.cpp" line="464"/>
         <source>Accessible Color Scheme</source>
-        <translation>Schéma de couleur accessible</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="515"/>
         <source>Open Link</source>
-        <translation>Ouvrir le lien</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="516"/>
         <source>Copy Link Address</source>
-        <translation>Copier l&apos;adresse du lien</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="520"/>
         <source>Send Email To...</source>
-        <translation>Envoyer un courriel à...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="521"/>
         <source>Copy Email Address</source>
-        <translation>Copier l&apos;adresse du courriel</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -89,60 +89,60 @@
     <message>
         <location filename="../qtermwidget.cpp" line="459"/>
         <source>Color Scheme Error</source>
-        <translation>Erreur du schéma des couleurs</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qtermwidget.cpp" line="460"/>
         <source>Cannot load color scheme: %1</source>
-        <translation>Impossible de charger le schéma de couleurs : %1</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>SearchBar</name>
     <message>
-        <location filename="../SearchBar.ui" line="14"/>
-        <source>SearchBar</source>
-        <translation>Barre de recherche</translation>
-    </message>
-    <message>
-        <location filename="../SearchBar.ui" line="20"/>
-        <source>X</source>
-        <translation>X</translation>
-    </message>
-    <message>
-        <location filename="../SearchBar.ui" line="32"/>
-        <source>Find:</source>
-        <translation>Trouver :</translation>
-    </message>
-    <message>
-        <location filename="../SearchBar.ui" line="42"/>
-        <source>&lt;</source>
-        <translation>&lt;</translation>
-    </message>
-    <message>
-        <location filename="../SearchBar.ui" line="54"/>
-        <source>&gt;</source>
-        <translation>&gt;</translation>
-    </message>
-    <message>
-        <location filename="../SearchBar.ui" line="66"/>
-        <source>...</source>
-        <translation>...</translation>
-    </message>
-    <message>
         <location filename="../SearchBar.cpp" line="40"/>
         <source>Match case</source>
-        <translation>Sensible à la casse</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../SearchBar.cpp" line="46"/>
         <source>Regular expression</source>
-        <translation>Expression régulière</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../SearchBar.cpp" line="50"/>
         <source>Highlight all matches</source>
-        <translation>Surbrillance de toutes les concordances</translation>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="14"/>
+        <source>SearchBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="20"/>
+        <source>X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="32"/>
+        <source>Find:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="42"/>
+        <source>&lt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="54"/>
+        <source>&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="66"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
