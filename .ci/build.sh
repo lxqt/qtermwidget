af7856ceb98bd5cbd2e6ff71a7f68f6562ca357c
Chih-Hsuan Yen <yan12125@gmail.com>
2021-02-17 23:19:21 +0800

    Use shared CI scripts

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,18 +1,7 @@
 set -ex
 
-cat >> /etc/pacman.conf <<EOF
-[archlinuxcn]
-Server = https://repo.archlinuxcn.org/\$arch
-SigLevel = Never
-EOF
+source shared-ci/prepare-archlinux.sh
 
-# Work-around the issue with glibc 2.33 on old Docker engines
-# Extract files directly as pacman is also affected by the issue
-patched_glibc=glibc-linux4-2.33-4-x86_64.pkg.tar.zst
-curl -LO https://repo.archlinuxcn.org/x86_64/$patched_glibc
-bsdtar -C / -xvf $patched_glibc
-
-pacman -Syu --noconfirm
 # See *depends in https://github.com/archlinuxcn/repo/blob/master/archlinuxcn/qtermwidget-git/PKGBUILD
 pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools python-pyqt5 python-sip4 sip4
 
