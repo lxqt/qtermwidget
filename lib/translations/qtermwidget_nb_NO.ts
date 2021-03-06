59d024520fb867964f52a1a36518b3c61a59c0f2
Allan Nordhøy <epost@anotheragency.no>
2021-03-06 23:06:55 +0100

    Weblate commits
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/nb_NO/
    Translation: LXQt Desktop/qtermwidget

--- lib/translations/qtermwidget_nb_NO.ts
+++ lib/translations/qtermwidget_nb_NO.ts
@@ -57,7 +57,7 @@
     <message>
         <location filename="../Vt102Emulation.cpp" line="1121"/>
         <source>No keyboard translator available.  The information needed to convert key presses into characters to send to the terminal is missing.</source>
-        <translation>Ingen tastaturoversetter er tilgjengelig. Informasjonen som trengs for å gjøre tastetrykk om til tegn å sende til terminalen mangler.</translation>
+        <translation>Ingen tastaturoversetter er tilgjengelig. Nødvendig info for å gjøre om tastetrykk til tegn å sende terminalen mangler.</translation>
     </message>
 </context>
 <context>
