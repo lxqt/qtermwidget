05cd020bcc8774409bfc978f33adb10d9672b03a
Yaron Shahrabani <sh.yaron@gmail.com>
2021-03-06 23:06:55 +0100

    Weblate commits
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/he/
    Translation: LXQt Desktop/qtermwidget

--- lib/translations/qtermwidget_he.ts
+++ lib/translations/qtermwidget_he.ts
@@ -39,12 +39,12 @@
     <message>
         <location filename="../TerminalDisplay.cpp" line="2741"/>
         <source>Paste multiline text</source>
-        <translation type="unfinished"></translation>
+        <translation>הדבקת מקבץ שורות</translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="2742"/>
         <source>Are you sure you want to paste this text?</source>
-        <translation type="unfinished"></translation>
+        <translation>להדביק את הטקסט הזה?</translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="3241"/>
@@ -65,7 +65,7 @@
     <message>
         <location filename="../TerminalDisplay.cpp" line="2748"/>
         <source>Show Details...</source>
-        <translation type="unfinished"></translation>
+        <translation>להציג פרטים…</translation>
     </message>
 </context>
 <context>
