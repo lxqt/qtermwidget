d682d30716d4e39373eb13c7f33c5027e697ba14
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-03-28 09:07:08 +0200

    Translations update from Weblate (#419)
    * Weblate commits
    
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/ja/
    Translation: LXQt Desktop/qtermwidget
    
    * Weblate commits
    
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/et/
    Translation: LXQt Desktop/qtermwidget
    
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>

--- lib/translations/qtermwidget_et.ts
+++ lib/translations/qtermwidget_et.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ja">
+<TS version="2.1" language="et">
 <context>
     <name>Konsole::Session</name>
     <message>
         <location filename="../Session.cpp" line="454"/>
         <source>Bell in session &apos;%1&apos;</source>
-        <translation>セッション &apos;%1&apos; のベル</translation>
+        <translation>Signaal sessioonis „%1“</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="606"/>
         <source>Session &apos;%1&apos; exited with status %2.</source>
-        <translation>セッション &apos;%1&apos; はステータス %2 で終了しました。</translation>
+        <translation>Sessioon „%1“ lõpetas töö olekuga %2.</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="608"/>
         <source>Session &apos;%1&apos; crashed.</source>
-        <translation>セッション &apos;%1&apos; がクラッシュしました。</translation>
+        <translation>Sessioon „%1“ jooksis kokku.</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="613"/>
         <source>Session &apos;%1&apos; exited unexpectedly.</source>
-        <translation>セッション &apos;%1&apos; が予期せず終了しました。</translation>
+        <translation>Sessioon „%1“ lõpetas ootamatult töö.</translation>
     </message>
 </context>
 <context>
@@ -29,27 +29,27 @@
     <message>
         <location filename="../TerminalDisplay.cpp" line="1279"/>
         <source>Size: XXX x XXX</source>
-        <translation>サイズ: XXX x XXX</translation>
+        <translation>Suurus: XXX x XXX</translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="1291"/>
         <source>Size: %1 x %2</source>
-        <translation>サイズ: %1 x %2</translation>
+        <translation>Suurus: %1 x %2</translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="2741"/>
         <source>Paste multiline text</source>
-        <translation>複数行テキストの貼り付け</translation>
+        <translation>Aseta mitmerealine tekst</translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="2742"/>
         <source>Are you sure you want to paste this text?</source>
-        <translation>このテキストを貼り付けますか？</translation>
+        <translation>Kas sa oled kindel, et soovid asetada seda teksti?</translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="3241"/>
         <source>&lt;qt&gt;Output has been &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;suspended&lt;/a&gt; by pressing Ctrl+S.  Press &lt;b&gt;Ctrl+Q&lt;/b&gt; to resume.&lt;/qt&gt;</source>
-        <translation>&lt;qt&gt;Ctrl+S を押して出力を&lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;中断&lt;/a&gt; しました 。再開するには &lt;b&gt;Ctrl+Q&lt;/b&gt; を押します。&lt;/qt&gt;</translation>
+        <translation>&lt;qt&gt;Vajutades Ctrl+S jääb väljundi kuvamine &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;pausile&lt;/a&gt;.  Jätkamiseks vajuta &lt;b&gt;Ctrl+Q&lt;/b&gt;.&lt;/qt&gt;</translation>
     </message>
 </context>
 <context>
@@ -57,7 +57,7 @@
     <message>
         <location filename="../Vt102Emulation.cpp" line="1121"/>
         <source>No keyboard translator available.  The information needed to convert key presses into characters to send to the terminal is missing.</source>
-        <translation>キーボードトランスレーターがありません。押されたキーを文字に変換してターミナルへ送信するために必要な情報がありません。</translation>
+        <translation>Klaviatuurisisendi tõlgendaja pole saadaval. Hetkel ei leidu teavet kuidas muuta klahvivajutused terminali saadetavateks tähemärkideks.</translation>
     </message>
 </context>
 <context>
@@ -65,7 +65,7 @@
     <message>
         <location filename="../TerminalDisplay.cpp" line="2748"/>
         <source>Show Details...</source>
-        <translation>詳細の表示...</translation>
+        <translation>Näita üksikasju...</translation>
     </message>
 </context>
 <context>
@@ -74,32 +74,32 @@
         <location filename="../ColorScheme.cpp" line="278"/>
         <location filename="../ColorScheme.cpp" line="293"/>
         <source>Un-named Color Scheme</source>
-        <translation>名前のない配色</translation>
+        <translation>Nimeta värvikombinatsioon</translation>
     </message>
     <message>
         <location filename="../ColorScheme.cpp" line="464"/>
         <source>Accessible Color Scheme</source>
-        <translation>アクセス可能な配色</translation>
+        <translation>Hõlpsalt kasutatav värvikombinatsioon</translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="515"/>
         <source>Open Link</source>
-        <translation>リンクを開く</translation>
+        <translation>Ava link</translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="516"/>
         <source>Copy Link Address</source>
-        <translation>リンクのアドレスをコピー</translation>
+        <translation>Kopeeri lingi aadress</translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="520"/>
         <source>Send Email To...</source>
-        <translation>メールを送信...</translation>
+        <translation>Saada e-kiri aadressile...</translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="521"/>
         <source>Copy Email Address</source>
-        <translation>メールアドレスをコピー</translation>
+        <translation>Kopeeri e-posti aadress</translation>
     </message>
 </context>
 <context>
@@ -107,12 +107,12 @@
     <message>
         <location filename="../qtermwidget.cpp" line="468"/>
         <source>Color Scheme Error</source>
-        <translation>配色のエラー</translation>
+        <translation>Värvikombinatsiooni viga</translation>
     </message>
     <message>
         <location filename="../qtermwidget.cpp" line="469"/>
         <source>Cannot load color scheme: %1</source>
-        <translation>配色をロードできません: %1</translation>
+        <translation>Värvikombinatsiooni laadimine ei õnnestu: %1</translation>
     </message>
 </context>
 <context>
@@ -120,32 +120,32 @@
     <message>
         <location filename="../SearchBar.cpp" line="40"/>
         <source>Match case</source>
-        <translation>大文字と小文字の区別</translation>
+        <translation>Tõstutundlik</translation>
     </message>
     <message>
         <location filename="../SearchBar.cpp" line="46"/>
         <source>Regular expression</source>
-        <translation>正規表現</translation>
+        <translation>Regulaaravaldis</translation>
     </message>
     <message>
         <location filename="../SearchBar.cpp" line="50"/>
         <source>Highlight all matches</source>
-        <translation>一致する全ての文字列を強調表示</translation>
+        <translation>Tõsta kõik vasted esile</translation>
     </message>
     <message>
         <location filename="../SearchBar.ui" line="14"/>
         <source>SearchBar</source>
-        <translation>サーチバー</translation>
+        <translation>Otsinguriba</translation>
     </message>
     <message>
         <location filename="../SearchBar.ui" line="20"/>
         <source>X</source>
-        <translation></translation>
+        <translation>X</translation>
     </message>
     <message>
         <location filename="../SearchBar.ui" line="32"/>
         <source>Find:</source>
-        <translation>検索:</translation>
+        <translation>Otsi:</translation>
     </message>
     <message>
         <location filename="../SearchBar.ui" line="42"/>
