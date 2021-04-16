9881fee203e9550dc159733555d0b16359651247
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-04-16 20:23:43 +0200

    Translations update from Weblate (#421)
    * Weblate commits
    
    Co-authored-by: Roman Volak <romanvolak@web.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/de/
    Translation: LXQt Desktop/qtermwidget
    
    * Weblate commits
    
    Co-authored-by: Pavel Fric <pavelfric@yahoo.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/cs/
    Translation: LXQt Desktop/qtermwidget
    
    Co-authored-by: Roman Volak <romanvolak@web.de>
    Co-authored-by: Pavel Fric <pavelfric@yahoo.com>

--- lib/translations/qtermwidget_cs.ts
+++ lib/translations/qtermwidget_cs.ts
@@ -39,12 +39,12 @@
     <message>
         <location filename="../TerminalDisplay.cpp" line="2741"/>
         <source>Paste multiline text</source>
-        <translation type="unfinished"></translation>
+        <translation>Vložit víceřádkový text</translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="2742"/>
         <source>Are you sure you want to paste this text?</source>
-        <translation type="unfinished"></translation>
+        <translation>Opravdu chcete vložit tento text?</translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="3241"/>
@@ -65,7 +65,7 @@
     <message>
         <location filename="../TerminalDisplay.cpp" line="2748"/>
         <source>Show Details...</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukázat podrobnosti...</translation>
     </message>
 </context>
 <context>
