0768633a83d32bae31a9337c63293de2a09fc859
Taras Panchenko <top39@ukr.net>
2021-09-12 17:08:04 +0200

    Weblate commits
    Co-authored-by: Taras Panchenko <top39@ukr.net>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/uk/
    Translation: LXQt Desktop/qtermwidget

---
+++ lib/translations/qtermwidget_uk.ts
@@ -0,0 +1,166 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>Konsole::Session</name>
+    <message>
+        <location filename="../Session.cpp" line="454"/>
+        <source>Bell in session &apos;%1&apos;</source>
+        <translation>Сигнал у сеансі &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="../Session.cpp" line="606"/>
+        <source>Session &apos;%1&apos; exited with status %2.</source>
+        <translation>Сеанс &apos;%1&apos; закінчився зі статусом %2.</translation>
+    </message>
+    <message>
+        <location filename="../Session.cpp" line="608"/>
+        <source>Session &apos;%1&apos; crashed.</source>
+        <translation>Збій сеансу &apos;%1&apos;.</translation>
+    </message>
+    <message>
+        <location filename="../Session.cpp" line="613"/>
+        <source>Session &apos;%1&apos; exited unexpectedly.</source>
+        <translation>Сеанс &apos;%1&apos; раптово закінчився.</translation>
+    </message>
+</context>
+<context>
+    <name>Konsole::TerminalDisplay</name>
+    <message>
+        <location filename="../TerminalDisplay.cpp" line="1279"/>
+        <source>Size: XXX x XXX</source>
+        <translation>Розмір: XXX x XXX</translation>
+    </message>
+    <message>
+        <location filename="../TerminalDisplay.cpp" line="1291"/>
+        <source>Size: %1 x %2</source>
+        <translation>Розмір: %1 x %2</translation>
+    </message>
+    <message>
+        <location filename="../TerminalDisplay.cpp" line="2741"/>
+        <source>Paste multiline text</source>
+        <translation>Вставити багаторядковий текст</translation>
+    </message>
+    <message>
+        <location filename="../TerminalDisplay.cpp" line="2742"/>
+        <source>Are you sure you want to paste this text?</source>
+        <translation>Ви впевнені, що хочете вставити цей текст?</translation>
+    </message>
+    <message>
+        <location filename="../TerminalDisplay.cpp" line="3241"/>
+        <source>&lt;qt&gt;Output has been &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;suspended&lt;/a&gt; by pressing Ctrl+S.  Press &lt;b&gt;Ctrl+Q&lt;/b&gt; to resume.&lt;/qt&gt;</source>
+        <translation>&lt;qt&gt;Виведення було &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;призупинено&lt;/a&gt; натисканням Ctrl+S.  Натисніть &lt;b&gt;Ctrl+Q&lt;/b&gt; для продовження.&lt;/qt&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Konsole::Vt102Emulation</name>
+    <message>
+        <location filename="../Vt102Emulation.cpp" line="1121"/>
+        <source>No keyboard translator available.  The information needed to convert key presses into characters to send to the terminal is missing.</source>
+        <translation>Не доступний транслятор клавіатури.  Відсутня  інформація, необхідна для перетворення натискання клавіш у символи та надсилання у термінал.</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <location filename="../TerminalDisplay.cpp" line="2748"/>
+        <source>Show Details...</source>
+        <translation>Показати подробиці...</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../ColorScheme.cpp" line="278"/>
+        <location filename="../ColorScheme.cpp" line="293"/>
+        <source>Un-named Color Scheme</source>
+        <translation>Колірна схема без назви</translation>
+    </message>
+    <message>
+        <location filename="../ColorScheme.cpp" line="464"/>
+        <source>Accessible Color Scheme</source>
+        <translation>Досяжна колірна схема</translation>
+    </message>
+    <message>
+        <location filename="../Filter.cpp" line="515"/>
+        <source>Open Link</source>
+        <translation>Відкрити посилання</translation>
+    </message>
+    <message>
+        <location filename="../Filter.cpp" line="516"/>
+        <source>Copy Link Address</source>
+        <translation>Копіювати адресу посилання</translation>
+    </message>
+    <message>
+        <location filename="../Filter.cpp" line="520"/>
+        <source>Send Email To...</source>
+        <translation>Відправити повідомлення е-пошти...</translation>
+    </message>
+    <message>
+        <location filename="../Filter.cpp" line="521"/>
+        <source>Copy Email Address</source>
+        <translation>Копіювати адресу е-пошти</translation>
+    </message>
+</context>
+<context>
+    <name>QTermWidget</name>
+    <message>
+        <location filename="../qtermwidget.cpp" line="468"/>
+        <source>Color Scheme Error</source>
+        <translation>Помилка колірної схеми</translation>
+    </message>
+    <message>
+        <location filename="../qtermwidget.cpp" line="469"/>
+        <source>Cannot load color scheme: %1</source>
+        <translation>Не можливо завантажити колірну схему</translation>
+    </message>
+</context>
+<context>
+    <name>SearchBar</name>
+    <message>
+        <location filename="../SearchBar.cpp" line="40"/>
+        <source>Match case</source>
+        <translation>Враховувати регістр</translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.cpp" line="46"/>
+        <source>Regular expression</source>
+        <translation>Регулярний вираз</translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.cpp" line="50"/>
+        <source>Highlight all matches</source>
+        <translation>Підсвітити всі збіги</translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="14"/>
+        <source>SearchBar</source>
+        <translation>ПанельПошуку</translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="20"/>
+        <source>X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="32"/>
+        <source>Find:</source>
+        <translation>Знайти:</translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="42"/>
+        <source>&lt;</source>
+        <translation>&lt;</translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="54"/>
+        <source>&gt;</source>
+        <translation>&gt;</translation>
+    </message>
+    <message>
+        <location filename="../SearchBar.ui" line="66"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+</context>
+</TS>
