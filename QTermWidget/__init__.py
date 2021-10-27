fe78bcd0034f325368d9ac5d74948bf219b4d8b8
JakobDev <jakobdev@gmx.de>
2021-10-27 19:31:50 +0200

    Add setup.py

---
+++ QTermWidget/__init__.py
@@ -0,0 +1,1 @@
+from .QTermWidget import QTermWidget
---
+++ setup.py
@@ -0,0 +1,63 @@
+from setuptools.command.build_ext import build_ext as build_ext_orig
+from setuptools import setup, Extension
+import pathlib
+import os
+
+
+class CMakeExtension(Extension):
+
+    def __init__(self, name):
+        # don't invoke the original build_ext for this special extension
+        super().__init__(name, sources=[])
+
+
+class build_ext(build_ext_orig):
+
+    def run(self):
+        for ext in self.extensions:
+            self.build_cmake(ext)
+        super().run()
+
+    def build_cmake(self, ext):
+        cwd = pathlib.Path().absolute()
+
+        # these dirs will be created in build_py, so if you don't have
+        # any python sources to bundle, the dirs will be missing
+        build_temp = pathlib.Path(self.build_temp)
+        build_temp.mkdir(parents=True, exist_ok=True)
+        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
+        extdir.mkdir(parents=True, exist_ok=True)
+
+        # example of cmake args
+        config = "Debug" if self.debug else "Release"
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + str(extdir.parent.absolute()),
+            "-DCMAKE_BUILD_TYPE=" + config,
+            "-D",
+            "QTERMWIDGET_BUILD_PYTHON_BINDING=ON"
+        ]
+
+        # example of build args
+        build_args = [
+            "--config", config,
+            "--", "-j4"
+        ]
+
+        os.chdir(str(build_temp))
+        self.spawn(["cmake", str(cwd)] + cmake_args)
+        if not self.dry_run:
+            self.spawn(["cmake", "--build", "."] + build_args)
+        # Troubleshooting: if fail on line above then delete all possible
+        # temporary CMake files including "CMakeCache.txt" in top level dir.
+        os.chdir(str(cwd))
+
+
+setup(
+    name="QTermWidget",
+    version='0.1',
+    packages=["QTermWidget"],
+    ext_modules=[CMakeExtension("QTermWidget/*")],
+    cmdclass={
+        "build_ext": build_ext,
+    }
+)
