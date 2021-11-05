652b20bdab5c75edef4904948b7ea70133cc495d
Chih-Hsuan Yen <yan12125@gmail.com>
2021-11-05 13:13:33 +0800

    Temporary workaround to make CI green (#451)
    See: https://github.com/lxqt/qtermwidget/pull/449#issuecomment-961165140

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,7 +3,11 @@ set -ex
 source shared-ci/prepare-archlinux.sh
 
 # See *depends in https://github.com/archlinuxcn/repo/blob/master/archlinuxcn/qtermwidget-git/PKGBUILD
-pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools python-pyqt5 python-sip4 sip4
+pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools python-sip4 sip4
+
+# Use older PyQt for now as 5.15.6 does not work with SIP 4
+# https://www.riverbankcomputing.com/pipermail/pyqt/2021-November/044345.html
+pacman -U --noconfirm https://archive.archlinux.org/packages/p/python-pyqt5/python-pyqt5-5.15.5-1-x86_64.pkg.tar.zst
 
 cmake -B build -S . \
     -DBUILD_EXAMPLE=ON \
