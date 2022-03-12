76b4e9f794eed73360641e3608a7bca2d5d9e154
Chih-Hsuan Yen <yan12125@gmail.com>
2022-03-12 11:02:30 +0800

    pyqt: move to SIP 5.x (#465)
    SIP 4.x is no longer supported [1].
    
    And drop CMake integration for PyQt bindings - it's too messy for SIP
    5.x
    
    [1] https://www.riverbankcomputing.com/software/sip/download

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,11 +3,12 @@ set -ex
 source shared-ci/prepare-archlinux.sh
 
 # See *depends in https://github.com/archlinuxcn/repo/blob/master/archlinuxcn/qtermwidget-git/PKGBUILD
-pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools python-sip4 sip4
+pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools python-pyqt5 pyqt-builder sip
 
-# TODO: re-enable PyQt bindings after switching to sip5
 cmake -B build -S . \
     -DBUILD_EXAMPLE=ON \
-    -DQTERMWIDGET_BUILD_PYTHON_BINDING=OFF \
     -DQTERMWIDGET_USE_UTEMPTER=ON
 make -C build
+
+cd pyqt
+CXXFLAGS="-I$PWD/../lib -I$PWD/../build/lib" LDFLAGS="-L$PWD/../build" sip-wheel --verbose
