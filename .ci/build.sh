7595d704aaf610f7c7e5285d61bb85012159fbca
Chih-Hsuan Yen <645432-yan12125@users.noreply.gitlab.com>
2023-02-23 12:43:29 +0800

    Expand the CI script with installation steps

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,12 +3,15 @@ set -ex
 source shared-ci/prepare-archlinux.sh
 
 # See *depends in https://github.com/archlinuxcn/repo/blob/master/archlinuxcn/qtermwidget-git/PKGBUILD
-pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools python-pyqt5 pyqt-builder sip
+pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools python-pyqt5 pyqt-builder sip python-installer
 
 cmake -B build -S . \
+    -DCMAKE_INSTALL_PREFIX=/usr \
     -DBUILD_EXAMPLE=ON \
     -DQTERMWIDGET_USE_UTEMPTER=ON
 make -C build
+make -C build install
 
 cd pyqt
 CXXFLAGS="-I$PWD/../lib -I$PWD/../build/lib" LDFLAGS="-L$PWD/../build" sip-wheel --verbose
+python -m installer --destdir="$pkgdir" *.whl
