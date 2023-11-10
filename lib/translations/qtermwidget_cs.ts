bdf5415e44008d8a9a4db21ed35f871275055b0f
Weblate <noreply@weblate.org>
2023-11-10 16:22:45 +0100

    Weblate commits
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Jalakas <anari.jalakas@gmail.com>
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: Juan <benitesjn@gmail.com>
    Co-authored-by: Pavel Fric <pavelfric@yahoo.com>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: cybercop <cybercop_montana@abv.bg>
    Co-authored-by: milotype <mail@milotype.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/es/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/hr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/uk/
    Translation: LXQt Desktop/qtermwidget

--- lib/translations/qtermwidget_cs.ts
+++ lib/translations/qtermwidget_cs.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../Session.cpp" line="606"/>
         <source>Session &apos;%1&apos; exited with code %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sezení &apos;%1&apos; bylo ukončeno s kódem %2.</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="608"/>
--- lib/translations/qtermwidget_fi.ts
+++ lib/translations/qtermwidget_fi.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../Session.cpp" line="606"/>
         <source>Session &apos;%1&apos; exited with code %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>Istunto &apos;%1&apos; päättyi koodilla %2.</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="608"/>
