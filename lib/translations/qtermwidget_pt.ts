d1d41ff9174d219bda94a13a5104f4c06d22cf81
Weblate <noreply@weblate.org>
2023-11-16 07:22:47 +0100

    Weblate commits
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Paolo Vigoroso <xpaolox@zoznam.sk>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/sk/
    Translation: LXQt Desktop/qtermwidget

--- lib/translations/qtermwidget_pt.ts
+++ lib/translations/qtermwidget_pt.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../Session.cpp" line="606"/>
         <source>Session &apos;%1&apos; exited with code %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>A sessão &apos;%1&apos; terminou com o código %2.</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="608"/>
