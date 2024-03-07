08fc85c1c4e08154bf438c183aa47f89e587e3b4
Doug <doug1234@users.noreply.github.com>
2024-03-07 09:35:16 -0500

    Fix the example.

--- examples/cpp/main.cpp
+++ examples/cpp/main.cpp
@@ -36,11 +36,11 @@ int main(int argc, char *argv[])
     QMenuBar *menuBar = new QMenuBar(mainWindow);
     QMenu *actionsMenu = new QMenu(QStringLiteral("Actions"), menuBar);
     menuBar->addMenu(actionsMenu);
-    actionsMenu->addAction(QStringLiteral("Find..."), QKeySequence(QLatin1String("Ctrl+Shift+F")), 
+    actionsMenu->addAction(QStringLiteral("Find..."), QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_F), 
         console, &QTermWidget::toggleShowSearchBar);
-    actionsMenu->addAction(QStringLiteral("Copy"), QKeySequence(QLatin1String("Ctrl+Shift+C")), 
+    actionsMenu->addAction(QStringLiteral("Copy"), QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_C), 
         console, &QTermWidget::copyClipboard);
-    actionsMenu->addAction(QStringLiteral("Paste"), QKeySequence(QLatin1String("Ctrl+Shift+V")), 
+    actionsMenu->addAction(QStringLiteral("Paste"), QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_V), 
         console, &QTermWidget::pasteClipboard);
     actionsMenu->addAction(QStringLiteral("About Qt"), &app, &QApplication::aboutQt);
     mainWindow->setMenuBar(menuBar);
