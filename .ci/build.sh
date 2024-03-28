c4cedb9f6f86d50ca4fd4b5c54ab66e2ebc05427
Doug <doug1234@users.noreply.github.com>
2024-03-28 09:27:44 -0400

    Temporary CI workaround suggested by marcusbritanicus.

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,6 +5,9 @@ source shared-ci/prepare-archlinux.sh
 # See *depends in https://github.com/archlinuxcn/repo/blob/master/archlinuxcn/qtermwidget-git/PKGBUILD
 pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt6-5compat qt6-tools python-pyqt6 pyqt-builder sip
 
+#Temporary workaround suggested by marcusbritanicus
+sudo ln -s /usr/bin/qmake6 /usr/bin/qmake
+
 cmake -B build -S . \
     -DBUILD_EXAMPLE=ON \
     -DQTERMWIDGET_USE_UTEMPTER=ON
