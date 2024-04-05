3cbbb16f50c83e1d874265dd030fce54c58c073d
Doug <doug1234@users.noreply.github.com>
2024-04-05 07:16:32 -0400

    Improved CI fix by marcusbritanicus

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,13 +5,10 @@ source shared-ci/prepare-archlinux.sh
 # See *depends in https://github.com/archlinuxcn/repo/blob/master/archlinuxcn/qtermwidget-git/PKGBUILD
 pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt6-5compat qt6-tools python-pyqt6 pyqt-builder sip
 
-#Temporary workaround suggested by marcusbritanicus
-sudo ln -s /usr/bin/qmake6 /usr/bin/qmake
-
 cmake -B build -S . \
     -DBUILD_EXAMPLE=ON \
     -DQTERMWIDGET_USE_UTEMPTER=ON
 make -C build
 
 cd pyqt
-CXXFLAGS="-I$PWD/../lib -I$PWD/../build/lib" LDFLAGS="-L$PWD/../build" sip-wheel --verbose
+CXXFLAGS="-I$PWD/../lib -I$PWD/../build/lib" LDFLAGS="-L$PWD/../build" sip-wheel --verbose --qmake=/usr/bin/qmake6
