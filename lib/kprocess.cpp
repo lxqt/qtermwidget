dc098f1ae1b29ce5f6712c2aed6b334a4561bd2f
Luís Pereira <luis.artur.pereira@gmail.com>
2024-05-03 15:47:39 +0100

    Use initializer list

--- lib/kprocess.cpp
+++ lib/kprocess.cpp
@@ -73,7 +73,7 @@ void KProcess::setNextOpenMode(QIODevice::OpenMode mode)
 
 void KProcess::clearEnvironment()
 {
-    setEnvironment(QStringList() << QString::fromLatin1(DUMMYENV));
+    setEnvironment(QStringList{QString::fromLatin1(DUMMYENV)});
 }
 
 void KProcess::setEnv(const QString &name, const QString &value, bool overwrite)
