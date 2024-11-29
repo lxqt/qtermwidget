2252b4627ac9faae43663ea785ed9706e4999e9f
Weblate <noreply@weblate.org>
2024-11-29 05:04:51 +0100

    Weblate commits
    Co-authored-by: Andi Chandler <andi@gowling.com>
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/kab/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qtermwidget/ko/
    Translation: LXQt Desktop/qtermwidget

--- lib/translations/qtermwidget_en_GB.ts
+++ lib/translations/qtermwidget_en_GB.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ko">
+<TS version="2.1" language="en_GB">
 <context>
     <name>Konsole::Session</name>
     <message>
         <location filename="../Session.cpp" line="448"/>
         <source>Bell in session &apos;%1&apos;</source>
-        <translation>세션 &apos;%1&apos;의 벨소리</translation>
+        <translation>Bell in session &apos;%1&apos;</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="600"/>
         <source>Session &apos;%1&apos; exited with code %2.</source>
-        <translation>&apos;%1&apos; 세션이 %2 코드로 종료되었습니다.</translation>
+        <translation>Session &apos;%1&apos; exited with code %2.</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="602"/>
         <source>Session &apos;%1&apos; crashed.</source>
-        <translation>&apos;%1&apos; 세션이 충돌했습니다.</translation>
+        <translation>Session &apos;%1&apos; crashed.</translation>
     </message>
     <message>
         <location filename="../Session.cpp" line="607"/>
         <source>Session &apos;%1&apos; exited unexpectedly.</source>
-        <translation>&apos;%1&apos; 세션이 예기치 않게 종료되었습니다.</translation>
+        <translation>Session &apos;%1&apos; exited unexpectedly.</translation>
     </message>
 </context>
 <context>
@@ -29,27 +29,27 @@
     <message>
         <location filename="../TerminalDisplay.cpp" line="1278"/>
         <source>Size: XXX x XXX</source>
-        <translation>크기: XXX x XXX</translation>
+        <translation>Size: XXX x XXX</translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="1290"/>
         <source>Size: %1 x %2</source>
-        <translation>크기: %1 x %2</translation>
+        <translation>Size: %1 x %2</translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="2790"/>
         <source>Paste multiline text</source>
-        <translation>여러 줄 텍스트 붙여넣기</translation>
+        <translation>Paste multiline text</translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="2791"/>
         <source>Are you sure you want to paste this text?</source>
-        <translation>이 텍스트를 붙여넣으시겠습니까?</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TerminalDisplay.cpp" line="3261"/>
         <source>&lt;qt&gt;Output has been &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;suspended&lt;/a&gt; by pressing Ctrl+S.  Press &lt;b&gt;Ctrl+Q&lt;/b&gt; to resume.&lt;/qt&gt;</source>
-        <translation>&lt;qt&gt;Ctrl+S를 눌러 출력이 &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;일시 중단&lt;/a&gt;되었습니다. 재개하려면 &lt;b&gt;Ctrl+Q&lt;/b&gt;를 눌러주세요.&lt;/qt&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -57,7 +57,7 @@
     <message>
         <location filename="../Vt102Emulation.cpp" line="1106"/>
         <source>No keyboard translator available.  The information needed to convert key presses into characters to send to the terminal is missing.</source>
-        <translation>키보드 번역기를 사용할 수 없습니다. 터미널에 보내기 위해 키 누름을 문자로 변환하는 데 필요한 정보가 누락되었습니다.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -65,7 +65,7 @@
     <message>
         <location filename="../TerminalDisplay.cpp" line="2797"/>
         <source>Show Details...</source>
-        <translation>세부정보 표시하기...</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -74,32 +74,32 @@
         <location filename="../ColorScheme.cpp" line="275"/>
         <location filename="../ColorScheme.cpp" line="290"/>
         <source>Un-named Color Scheme</source>
-        <translation>이름 없는 색상 구성표</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ColorScheme.cpp" line="461"/>
         <source>Accessible Color Scheme</source>
-        <translation>접근 가능한 색상 구성표</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="520"/>
         <source>Open Link</source>
-        <translation>링크 열기</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="521"/>
         <source>Copy Link Address</source>
-        <translation>링크 주소 복사하기</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="525"/>
         <source>Send Email To...</source>
-        <translation>이메일 보내기...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Filter.cpp" line="526"/>
         <source>Copy Email Address</source>
-        <translation>이메일 주소 복사하기</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -107,12 +107,12 @@
     <message>
         <location filename="../qtermwidget.cpp" line="465"/>
         <source>Color Scheme Error</source>
-        <translation>색상 구성표 오류</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qtermwidget.cpp" line="466"/>
         <source>Cannot load color scheme: %1</source>
-        <translation>색상 구성표 불러올 수 없음: %1</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -120,47 +120,47 @@
     <message>
         <location filename="../SearchBar.cpp" line="39"/>
         <source>Match case</source>
-        <translation>대소문자 일치</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../SearchBar.cpp" line="45"/>
         <source>Regular expression</source>
-        <translation>정규식</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../SearchBar.cpp" line="49"/>
         <source>Highlight all matches</source>
-        <translation>모든 일치 항목 강조표시</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../SearchBar.ui" line="14"/>
         <source>SearchBar</source>
-        <translation>검색 창</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../SearchBar.ui" line="20"/>
         <source>X</source>
-        <translation>X</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../SearchBar.ui" line="32"/>
         <source>Find:</source>
-        <translation>찾기:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../SearchBar.ui" line="42"/>
         <source>&lt;</source>
-        <translation>&lt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../SearchBar.ui" line="54"/>
         <source>&gt;</source>
-        <translation>&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../SearchBar.ui" line="66"/>
         <source>...</source>
-        <translation>...</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
