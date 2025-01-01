29b4ff8ee674b71e78f7453842e03bd848864a13
Karol Maksymowicz <ja@loliting.dev>
2025-01-01 03:12:00 +0100

    add appdir/color-schemes as a search path for color schemes on windows

--- lib/tools.cpp
+++ lib/tools.cpp
@@ -71,7 +71,7 @@ const QStringList get_color_schemes_dirs()
     if (d.exists())
         rval << k.append(QLatin1Char('/'));
 
-#ifdef Q_OS_MAC
+#if defined(Q_OS_MAC) || defined(Q_OS_WIN)
     // subdir in the app location
     d.setPath(QCoreApplication::applicationDirPath() + QLatin1String("/color-schemes/"));
     //qDebug() << d.path();
@@ -81,6 +81,8 @@ const QStringList get_color_schemes_dirs()
             rval.clear();
         rval << (QCoreApplication::applicationDirPath() + QLatin1String("/color-schemes/"));
     }
+#endif
+#ifdef Q_OS_MAC
     d.setPath(QCoreApplication::applicationDirPath() + QLatin1String("/../Resources/color-schemes/"));
     if (d.exists())
     {
