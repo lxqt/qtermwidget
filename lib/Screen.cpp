2e0d4dde917fe6b59bae6aa07887c01ed927f53b
Karol Maksymowicz <ja@loliting.dev>
2025-01-01 02:28:33 +0100

    remove unnecessary unistd.h includes
    unistd.h is not avaliable on MSVC, but we do not use any functions from it

--- lib/Screen.cpp
+++ lib/Screen.cpp
@@ -26,7 +26,6 @@
 // Standard
 #include <cstdio>
 #include <cstdlib>
-#include <unistd.h>
 #include <cstring>
 #include <cctype>
 
@@ -1350,6 +1349,8 @@ int Screen::copyLineToStream(int line ,
         }
 
         // count cannot be any greater than length
+        if(count > length - start)
+            return 0;
         count = qBound(0,count,length-start);
 
         Q_ASSERT( screenLine < lineProperties.count() );
