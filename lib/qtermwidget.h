61b838652bd3fc2de9435305897e9a64dd0c5882
Shawn W Dunn <sfalken@kalpadesktop.org>
2026-04-30 14:23:53 -0700

    update fsf address

--- lib/qtermwidget.h
+++ lib/qtermwidget.h
@@ -11,9 +11,8 @@
     Library General Public License for more details.
 
     You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+    along with this library; see the file COPYING.LIB.  If not, see
+    <https://www.gnu.org/licenses/>.
 */
 
 
